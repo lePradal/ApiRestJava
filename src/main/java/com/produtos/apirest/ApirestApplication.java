53fbf572275564ed8f5cbb7783913fd65f7b157b
Leandro Pradal Lopes <le.pradal.lopes@gmail.com>
2020-07-30 02:48:09 -0300

    Adição de logger no Application

--- src/main/java/com/produtos/apirest/ApirestApplication.java
+++ src/main/java/com/produtos/apirest/ApirestApplication.java
@@ -1,12 +1,17 @@
 package com.produtos.apirest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApirestApplication {
+	static Logger LOGGER = LoggerFactory.getLogger("ApirestApplicationLogger");
+
 
 	public static void main(String[] args) {
+		LOGGER.info("Initialing application.");
 		SpringApplication.run(ApirestApplication.class, args);
 	}
 
